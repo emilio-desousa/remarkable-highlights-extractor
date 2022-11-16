2241f8ae3996ab16115192623abad1590f8f000a
Emilio DE SOUSA <desousa.emilio.info@gmail.com>
2022-11-16 23:05:57 +0100

    feat: show highlights for a document

--- highlights_extractor/app/main_page.py
+++ highlights_extractor/app/main_page.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
-from highlights_extractor.file_reader import LocalFileReader
-from highlights_extractor.models import DocumentMetadata
+from highlights_extractor.models import (
+    DocumentHighlights,
+    DocumentMetadata,
+    PageHighlights,
+)
+from highlights_extractor.repository.file_reader import LocalFileReader
 
 local_fs = LocalFileReader()
 metadata_files = local_fs.read_all_metadata_files(["visibleName"])
@@ -10,7 +14,14 @@ documents_metadata = [
 ]
 
 with st.sidebar:
-    st.selectbox(
-        "document",
-        [document_metadata.document_name for document_metadata in documents_metadata],
-    )
+    document = st.selectbox("document", list(documents_metadata))
+if document:
+    highlights_files = local_fs.read_document_highlights(document.document_id)
+    all_highlights = []
+    for highlight_file in highlights_files:
+        page_highlights = PageHighlights(highlight_file)
+        all_highlights.append(page_highlights)
+        st.write(str(page_highlights))
+        st.write(page_highlights.raw_file.file_path)
+    doc_highlights = DocumentHighlights(all_highlights)
+    st.write(str(doc_highlights))
