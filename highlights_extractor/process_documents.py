2aa1bdb1d1845c1fe19a9887e7f4b24fef2637f1
Emilio DE SOUSA <desousa.emilio.info@gmail.com>
2023-01-03 19:32:59 +0100

    refactor(process_documents): Reorder function + delete old functions

--- highlights_extractor/process_documents.py
+++ highlights_extractor/process_documents.py
@@ -10,8 +10,8 @@ import pandas as pd
 from PIL import Image
 
 from highlights_extractor.config.exceptions import DocumentNotProcessableError
-from highlights_extractor.models import DocumentContent, DocumentMetadata
-from highlights_extractor.repository.file_reader import FileReader, RawHighlightFile
+from highlights_extractor.models import DocumentContent
+from highlights_extractor.repository.file_reader import RawHighlightFile
 
 
 @dataclass
@@ -29,27 +29,6 @@ class PDFExtractor:
     def __init__(self, document_path: Path, document_name: str) -> None:
         self.reader = self._get_fitz_reader(document_path)
         self.document_name = document_name
-
-    def _get_raw_table_of_contents(self) -> List[TableOfContentItem]:
-        if table_of_content := self.reader.get_toc():  # type: ignore
-            return [TableOfContentItem(*item) for item in table_of_content]
-
-        raise DocumentNotProcessableError(
-            f"Document: {self.document_name} does not have a table of contents"
-            "or it cannot be found."
-        )
-
-    @staticmethod
-    def _get_table_of_contents_df(
-        raw_table_of_content: List[TableOfContentItem],
-    ) -> pd.DataFrame:
-        table_of_contents = pd.DataFrame(
-            raw_table_of_content, columns=["level", "title", "page"]
-        )
-        table_of_contents["title"] = table_of_contents["title"].apply(
-            clean_table_content_item_text
-        )
-        return table_of_contents
 
     def get_chapter_title(self, page_number: int) -> str:
         """Get the chapter title for a given page number.
@@ -74,29 +53,69 @@ class PDFExtractor:
         table_of_content_items = self._get_raw_table_of_contents()
         table_of_contents = self._get_table_of_contents_df(table_of_content_items)
 
-        table_of_contents = table_of_contents.assign(
-            difference_between_page_and_chapters=lambda df: df["page"] - page_number
+        only_chapters_before_current_page_df = (
+            self._get_only_chapters_before_current_page(page_number, table_of_contents)
         )
-        only_chapters_with_page_number_less_than_current_page_df = table_of_contents[
-            table_of_contents["difference_between_page_and_chapters"] <= 0
-        ]
-
-        if only_chapters_with_page_number_less_than_current_page_df.empty:
+        if only_chapters_before_current_page_df.empty:
             raise DocumentNotProcessableError(
-                "Could not find chapter title "
-                f"for page: {page_number} in {self.document_name}"
+                f"Could not find chapter title for page: {page_number} in"
+                f" {self.document_name}"
             )
-
-        chapter_corresponding_to_the_current_page = table_of_contents.iloc[
-            only_chapters_with_page_number_less_than_current_page_df[
-                "difference_between_page_and_chapters"
-            ].argmax()
-        ]
-
+        chapter_corresponding_to_the_current_page = (
+            self._get_most_closest_chapter_before_page(
+                only_chapters_before_current_page_df
+            )
+        )
         chapter_title = chapter_corresponding_to_the_current_page["title"]
         if isinstance(chapter_title, str):
             return chapter_title
         return list(chapter_title)[0]
+
+    def _get_most_closest_chapter_before_page(
+        self, only_chapters_before_current_page_df: pd.DataFrame
+    ) -> pd.DataFrame:
+        chapter_corresponding_to_the_current_page = (
+            only_chapters_before_current_page_df.iloc[
+                only_chapters_before_current_page_df[
+                    "difference_between_page_and_chapters"
+                ].argmax()
+            ]
+        )
+        return chapter_corresponding_to_the_current_page
+
+    def _get_only_chapters_before_current_page(
+        self, page_number: int, table_of_contents: pd.DataFrame
+    ) -> pd.DataFrame:
+        tmp_column = "difference_between_page_and_chapters"
+        table_of_contents = table_of_contents.assign(
+            **{tmp_column: lambda df: df["page"] - page_number}
+        )
+        only_chapters_with_page_number_less_than_current_page_df = table_of_contents[
+            table_of_contents[tmp_column] <= 0
+        ]
+
+        return only_chapters_with_page_number_less_than_current_page_df
+
+    def _get_raw_table_of_contents(self) -> List[TableOfContentItem]:
+        if table_of_content := self.reader.get_toc():  # type: ignore
+            return [TableOfContentItem(*item) for item in table_of_content]
+
+        raise DocumentNotProcessableError(
+            f"Document: {self.document_name} does not have a table of contentsor it"
+            " cannot be found."
+        )
+
+    @staticmethod
+    def _get_table_of_contents_df(
+        raw_table_of_content: List[TableOfContentItem],
+    ) -> pd.DataFrame:
+        table_of_contents = pd.DataFrame(
+            raw_table_of_content, columns=["level", "title", "page"]
+        )
+        table_of_contents["title"] = table_of_contents["title"].apply(
+            clean_table_content_item_text
+        )
+        return table_of_contents
 
     def _get_fitz_reader(self, document_path: Path) -> fitz.Document:
         return fitz.Document(document_path)
@@ -147,20 +166,6 @@ def clean_table_content_item_text(text: str) -> str:
         r"^[0-9. ]*", "", text_without_special_chars
     )
     return text_without_special_chars_and_chapter_number
-
-
-def get_all_file_names(file_reader: FileReader) -> list[DocumentMetadata]:
-    metadata_files = file_reader.read_all_metadata_files(["visibleName"])
-    documents_metadata = [
-        DocumentMetadata(metadata_file) for metadata_file in metadata_files
-    ]
-    return documents_metadata
-
-
-def get_document_id_from_metadata_documents(
-    documents_metadata: list[DocumentMetadata], index: int = 6
-) -> DocumentMetadata:
-    return documents_metadata[index]
 
 
 def get_page_number(document_content: DocumentContent, page: RawHighlightFile) -> int:
